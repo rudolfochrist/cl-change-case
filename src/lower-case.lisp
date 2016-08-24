7c3642a81913aacb49ab505d0ca5b3e2d68066bc
Sebastian Christ <rudolfo.christ@gmail.com>
2016-08-24 11:21:40 +0200

    Add docstrings.

--- src/lower-case.lisp
+++ src/lower-case.lisp
@@ -5,12 +5,14 @@
 (define-constant +empty-string+ "" :test #'string=)
 
 (defun lower-case (string)
+  "Downcase each character in STRING."
   (if string
       (string-downcase string)
       +empty-string+))
 
 
 (defun lower-case-first (string)
+  "Downcase the first character in STRING."
   (if (zerop (length string))
       +empty-string+
       (let ((copy (copy-seq string)))
@@ -19,5 +21,6 @@
         copy)))
 
 (defun string-lower-case-p (string)
+  "Tests if each character in STRING has lower case."
   (every #'identity (map 'list #'lower-case-p string)))
 
